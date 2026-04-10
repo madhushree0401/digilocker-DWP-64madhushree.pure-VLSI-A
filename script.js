88a17dc20ca5fb51d0066daa532b8ed23b330fe4
madhushree0401 <madhushreepure16@gmail.com>
2026-04-10 16:37:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,29 @@
-// OTP
-function sendOTP() {
-    let mobile = document.getElementById("mobile").value;
+// VERIFY button
+document.getElementById("verifyBtn").addEventListener("click", function () {
+    let mobile = document.querySelector("input[placeholder='Mobile Number']").value;
 
-    if (mobile.length != 10) {
-        alert("Enter valid mobile number");
+    if (mobile === "") {
+        alert("Please enter mobile number");
+    } else if (mobile.length !== 10) {
+        alert("Enter valid 10 digit mobile number");
     } else {
-        alert("OTP Sent");
+        alert("OTP Sent Successfully!");
     }
-}
+});
 
-// Login
-function login() {
-    let otp = document.getElementById("otp").value;
 
-    if (otp === "1234")
-         {
-            window.location.href = "dashboard.html";
+// CONTINUE button
+document.getElementById("continueBtn").addEventListener("click", function () {
+    let otp = document.querySelector("input[placeholder='Enter OTP']").value;
+    let username = document.querySelector("input[placeholder='Username']").value;
+    let password = document.querySelector("input[placeholder='Password']").value;
+
+    if (otp === "" || username === "" || password === "") {
+        alert("Please fill all details");
+    } else {
+        alert("Signup Successful!");
         
+        // 👉 YEH LINE IMPORTANT HAI
+        window.location.href = "dashboard.html";
     }
-     else 
-        {
-        alert("Wrong OTP");
-    }
-}
-
-// Fetch Document
-function fetchDoc() {
-    let name = document.getElementById("name").value;
-    let dept = document.getElementById("dept").value;
-
-    if (name === "") {
-        alert("Enter details");
-    } else {
-        localStorage.setItem("name", name);
-        localStorage.setItem("dept", dept);
-        window.location.href = "document.html";
-    }
-}
-
-// Show Document
-window.onload = function () {
-    if (document.getElementById("userName")) {
-        document.getElementById("userName").innerText = localStorage.getItem("name");
-        document.getElementById("docType").innerText = localStorage.getItem("dept");
-    }
-}
-
-// Actions
-function download() {
-    alert("Document Downloaded");
-}
-
-function share() {
-    alert("Document Shared");
-}
+});
